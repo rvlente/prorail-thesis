9a5b3988388a07ecb84ef5ee4f965387d502de04
Robert van Lente <robertvlente@gmail.com>
2023-12-17 23:25:30 +0000

    Update run.sh.

--- index-benchmarking/run.sh
+++ index-benchmarking/run.sh
@@ -17,12 +17,17 @@ mkdir -p heapprofile
 cmake ..
 cmake --build . --target $1
 
-./$@
+cd ..
 
+./build/$@
+
+cp -r build/heapprofile heapprofiles/$1-$(date +%Y%m%dT%H%M%S)
+
+set +e
 echo "Analyzing heap profile dumps..."
+pprof --text --inuse_space $1 heapprofile/$(ls heapprofile | grep s2pointindex | tail -1) | grep build_s2pointindex
+pprof --text --inuse_space $1 heapprofile/$(ls heapprofile | grep s2shapeindex | tail -1) | grep build_s2shapeindex
 pprof --text --inuse_space $1 heapprofile/$(ls heapprofile | grep strtree | tail -1) | grep build_strtree
 pprof --text --inuse_space $1 heapprofile/$(ls heapprofile | grep quadtree | tail -1) | grep build_quadtree
-
-cd ..
-cp -r build/heapprofile heapprofiles/$1-$(date +%Y%m%dT%H%M%S)
+set -e
 
